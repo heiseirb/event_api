09a5b2260d72b392a57ef0935a3d20709ee45974
tashiro <33741858+tashirosota@users.noreply.github.com>
2019-08-14 23:06:10 +0900

    Update event_schedules_controller.rb

--- app/controllers/client/event_schedules_controller.rb
+++ app/controllers/client/event_schedules_controller.rb
@@ -20,7 +20,7 @@ class Client::EventSchedulesController < Client::ApplicationController
 
   # 申し込み
   def apply
-    render json:  @event_schedule.user_to_event_schedules.create!(user_id: params[user_id]), include: '**'
+    render json:  @event_schedule.user_to_event_schedules.create!(user_id: params[:user_id]), include: '**'
   end
 
   private
